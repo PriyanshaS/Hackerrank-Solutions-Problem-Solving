39ebb50ab68bd9988b32e74f3ea6d0c03760e564
Priyansha Singhal <singhalpriyansha@gmail.com>
2021-09-29 22:09:22 +0530

    Create ps3.cpp

---
+++ ps3.cpp
@@ -0,0 +1,166 @@
+Question -
+  
+  Given an array of bird sightings where every element represents a bird type id, determine the id of the most frequently sighted type. If more than 1 type has been spotted that maximum amount, return the smallest of their ids.
+
+Example arr=[1,1,2,2,3]
+
+There are two each of types 1 and 2 , and one sighting of type 3. Pick the lower of the two types seen twice: type 1.
+
+Function Description
+
+Complete the migratoryBirds function in the editor below.
+
+migratoryBirds has the following parameter(s):
+
+int arr[n]: the types of birds sighted
+Returns
+
+int: the lowest type id of the most frequently sighted birds
+Input Format
+
+The first line contains an integer, n, the size of arr.
+The second line describes  as  space-separated integers, each a type number of the bird sighted.
+
+Constraints
+
+It is guaranteed that each type is 1 ,2 ,3 ,4 , or 5.
+Sample Input 0
+
+6
+1 4 4 4 5 3
+Sample Output 0
+
+4
+Explanation 0
+
+The different types of birds occur in the following frequencies:
+
+Type :  bird
+Type :  birds
+Type :  bird
+Type :  birds
+Type :  bird
+The type number that occurs at the highest frequency is type , so we print  as our answer.
+
+Sample Input 1
+
+11
+1 2 3 4 5 4 3 2 1 3 4
+Sample Output 1
+
+3
+Explanation 1
+
+The different types of birds occur in the following frequencies:
+
+Type : 
+Type : 
+Type : 
+Type : 
+Type : 
+Two types have a frequency of , and the lower of those is type .#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'migratoryBirds' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+int migratoryBirds(vector<int> arr) {
+int array[6];
+for(int i =0;i<6;i++){
+    array[i]=0;
+}
+for(int i =0;i<(int)arr.size();i++){
+    array[arr[i]]++;
+}
+int index=0;
+    int max=array[0];
+
+for(int i =0;i<6;i++){
+    if(max<array[i]){
+        max=array[i];
+        index=i;
+       // cout<<index;
+       // cout<<max;
+    } 
+}
+return index;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string arr_count_temp;
+    getline(cin, arr_count_temp);
+
+    int arr_count = stoi(ltrim(rtrim(arr_count_temp)));
+
+    string arr_temp_temp;
+    getline(cin, arr_temp_temp);
+
+    vector<string> arr_temp = split(rtrim(arr_temp_temp));
+
+    vector<int> arr(arr_count);
+
+    for (int i = 0; i < arr_count; i++) {
+        int arr_item = stoi(arr_temp[i]);
+
+        arr[i] = arr_item;
+    }
+
+    int result = migratoryBirds(arr);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
